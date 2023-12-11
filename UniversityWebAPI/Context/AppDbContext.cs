9bdcc67f20766e5e4b48554cfab5e4ff45298da7
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2023-12-11 22:51:16 +0530

    Add project files.

---
+++ UniversityWebAPI/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityWebAPI.Models;
+
+namespace UniversityWebAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+
+        //helps to take the entity
+        protected override void  OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
---
+++ UniversityWebAPI/Controllers/UserController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UniversityWebAPI.Context;
+using UniversityWebAPI.Helpers;
+using UniversityWebAPI.Models;
+
+namespace UniversityWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public UserController(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+
+        }
+        [HttpPost("authenticate")]
+
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == userObj.Email);
+
+            if (user == null)
+                return NotFound(new { Meassage = "User Not Found" });
+
+            if(!PasswordHasher.VerifyPassword(userObj.Password, user.Password))
+            {
+                return BadRequest(new {Message = "Password is incorrect"});
+            }
+
+            user.Token = CreateJwt(user);
+            return Ok(new
+            {
+                Token =user.Token,
+                Message = "Login Success!"
+            });
+
+
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if(userObj == null)
+                return BadRequest();
+            userObj.Password = PasswordHasher.HashPassword(userObj.Password);
+            userObj.Role = "User";
+            userObj.Token = "";
+            await _context.Users.AddAsync(userObj); 
+            await _context.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User successfully registered!"
+            });
+
+
+        }
+
+        private string CreateJwt(User user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("veryverysecret...");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim(ClaimTypes.Email, user.Email)
+
+            });
+
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);   
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> GetAllUser()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+    }
+}
